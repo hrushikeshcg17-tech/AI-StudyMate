b30925bfae556268e2b934c9d07387556cac31bc
hrushikeshcg17-tech <hrushikesh.cg17@gmail.com>
2026-07-16 23:56:40 +0530

    Delete frontend directory

--- frontend/src/pages/Workspace.jsx
+++
@@ -1,7 +0,0 @@
-export default function Workspace() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">Workspace</h1>
-    </div>
-  );
-}
